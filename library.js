70dc758851937e21025e5460beff2da552be24ea
Liam's Apps <liams.systems@gmail.com>
2025-05-29 06:53:11 -0400

    Add buttons (1.open form, 2.add book) using built-in validation

--- library.js
+++ library.js
@@ -1,10 +1,21 @@
 const myLibrary = [];
-const container = document.querySelector("#container");
+// const container = document.querySelector("#container");
+const books = document.querySelector("#books");
+const btnAdd = document.querySelector("#add");
+const myForm = document.querySelector("#my_form");
+const bTitle = document.querySelector("#book_title");
+const bAuthor = document.querySelector("#book_author");
+const bPages = document.querySelector("#book_pages");
+const bRead = document.querySelector("#book_read");
+const btnSubmit = document.querySelector("#submit");
+const btnCancel = document.querySelector("#cancel");
+
+const bookInfo = document.querySelectorAll(".book_info");
 
 function GenerateGuid() {
     if (self && self.crypto && typeof self.crypto.randomUUID === 'function') {
         const uuid = self.crypto.randomUUID(); 
-        console.log(uuid);
+        // console.log(uuid);
         return uuid;
     } else {
         console.log("self.crypto not available");
@@ -26,55 +37,132 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     console.log('in addBookToLibrary');
     myLibrary.push(new Book(title, author, pages, read));    
+    // myLibrary.push(new Book(title, author, pages, (read === "yes" ? true : false)));    
 }
 
-addBookToLibrary("Utopia", "Sir Thomas More", 359, true);
-addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, true);
-addBookToLibrary("Utopia", "Sir Thomas More", 359, true);
-addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, true);
-addBookToLibrary("Utopia", "Sir Thomas More", 359, true);
-addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, true);
-addBookToLibrary("Utopia", "Sir Thomas More", 359, true);
-addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, true);
-addBookToLibrary("Utopia", "Sir Thomas More", 359, true);
-addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, true);
-addBookToLibrary("Utopia", "Sir Thomas More", 359, true);
-addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, true);
+// test data
+addBookToLibrary("Utopia", "Sir Thomas More", 359, "yes");
+addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, "no");
+// addBookToLibrary("Utopia", "Sir Thomas More", 359, "yes");
+// addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, "yes");
+// addBookToLibrary("Utopia", "Sir Thomas More", 359, true);
+// addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, "no");
+// addBookToLibrary("Utopia", "Sir Thomas More", 359, true);
+// addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, "yes");
+// addBookToLibrary("Utopia", "Sir Thomas More", 359, true);
+// addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, "yes");
+// addBookToLibrary("Utopia", "Sir Thomas More", 359, true);
+// addBookToLibrary("Pillars of the Earth", "Ken Follett", 806, "no");
 
-myLibrary.forEach((item) => {            
-    console.log('start of myLibrary.forEach');
-
-    const bookCard = document.createElement("div");
-    bookCard.className = "card";
-    const list = document.createElement("ul");
-
-    const bookID = document.createElement("li");
-    bookID.textContent = item.id;
-
-    const bookTitle = document.createElement("li");
-    bookTitle.textContent = item.title;
-
-    const bookAuthor = document.createElement("li");
-    bookAuthor.textContent = item.author;
-
-    const bookPages = document.createElement("li");
-    bookPages.textContent = item.pages;
-
-    const bookRead = document.createElement("li");
-    bookRead.textContent = item.read;
-
-    console.log(item.id);
-    console.log(item.title);
-    console.log(item.author);
-    console.log(item.pages);
-    console.log(item.read);
-
-    const items = [bookID, bookTitle, bookAuthor, bookPages, bookRead];
-    console.log(items[0]);
-    items.forEach(item => list.appendChild(item));
-
-    bookCard.appendChild(list);
-    container.appendChild(bookCard);
-
-    console.log('end of myLibrary.forEach');
+document.addEventListener("DOMContentLoaded", (event) => {
+    myForm.style.cssText = "display: none; visibility: hidden";
+    // bookInfo.forEach(item => item.disabled = true);
+    loadBooks();
 });
+
+myForm.addEventListener("submit", (event) => {
+
+    console.log("in myForm submit listener");
+    // If the form is invalid, do not proceed        
+    if (!myForm.checkValidity()) {
+        // NOT DISPLAYED !!!
+        // alert("Please fill out all required fields correctly.");
+        // console.log("Please fill out all required fields correctly.");
+        event.preventDefault(); // Prevent submission
+        // return;            
+    }
+    else {
+        // DISPLAYED
+        // alert("Form is valid and will be submitted.");
+        console.log("Form is valid and will be submitted.");
+        console.log(bRead.value);
+        addBookToLibrary(bTitle.value, bAuthor.value, bPages.value, bRead.value);
+        
+        event.preventDefault();
+
+        formReset();
+        formDisplay();
+        loadBooks();
+    }        
+});
+
+
+btnAdd.addEventListener("click", formDisplay);
+// btnCancel.addEventListener("click", formDisplay);
+btnCancel.addEventListener("click", () => {
+    formDisplay();
+    formReset();
+});
+
+
+
+function formDisplay() {
+    // console.log("in formDisplay");
+    // console.log(`btnAdd.disabled: ${btnAdd.disabled}`);
+    if (btnAdd.disabled === false) {
+        console.log(`btnAdd.disabled: ${btnAdd.disabled}`);
+        btnAdd.disabled = true;        
+        myForm.style.cssText = "display: block; visibility: visible";
+        bookInfo.forEach(item => item.disabled = false);
+    }
+    else {
+        btnAdd.disabled = false;
+        myForm.style.cssText = "display: none; visibility: hidden";
+        bookInfo.forEach(item => item.disabled = true);
+
+    }    
+}
+
+function formReset() {
+    bTitle.value = "";
+    bAuthor.value = "";
+    bPages.value = "";
+    bRead.value = "";
+}
+
+function loadBooks() {
+    while (books.hasChildNodes()) {
+        books.removeChild(books.firstChild);
+    }
+
+    myLibrary.forEach((item) => {            
+        console.log('start of myLibrary.forEach');
+
+        const bookCard = document.createElement("div");
+        bookCard.className = "card";
+        const list = document.createElement("ul");
+
+        // const bookID = document.createElement("li");
+        // bookID.textContent = item.id;
+
+        const bookTitle = document.createElement("li");
+        bookTitle.textContent = item.title;
+
+        const bookAuthor = document.createElement("li");
+        bookAuthor.textContent = item.author;
+
+        const bookPages = document.createElement("li");
+        bookPages.textContent = item.pages;
+
+        const bookRead = document.createElement("li");        
+        bookRead.textContent = item.read;
+        
+        // console.log(item.id);
+        // console.log(item.title);
+        // console.log(item.author);
+        // console.log(item.pages);
+        // console.log(item.read);
+
+        // const items = [bookID, bookTitle, bookAuthor, bookPages, bookRead];
+        const items = [bookTitle, bookAuthor, bookPages, bookRead];
+        console.log(items[0]);
+        items.forEach(item => list.appendChild(item));
+
+        bookCard.appendChild(list);
+        // container.appendChild(bookCard);
+        books.appendChild(bookCard);
+
+        console.log('end of myLibrary.forEach');
+    });
+}
+
